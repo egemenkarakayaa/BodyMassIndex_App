cfffc50297170f1c5b45d912ae380d9ad3d62b9e
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-12-25 22:21:15 +0300

    Some additions have been made to the project regarding Firebase and Cocoapods.

--- BodyMassIndex_App/ViewController.swift
+++ BodyMassIndex_App/ViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    private let label: UILabel = {
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Log In"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+    private let emailField: UITextField = {
+        
+        let emailField = UITextField()
+        emailField.placeholder = "Email Address"
+        emailField.layer.borderWidth = 1
+        emailField.layer.borderColor = UIColor.black.cgColor
+        return emailField
+    }()
+    
+   /* private let label:  UITextField = {
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Log In"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+    private let label: UIButton = {
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Log In"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+*/
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        view.addSubview(label)
     }
 
 
