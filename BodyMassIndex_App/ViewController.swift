d9f7aafd387d3e915f21f6d090205a5515502295
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-12-26 11:19:02 +0300

    Added login page components.

--- BodyMassIndex_App/ViewController.swift
+++ BodyMassIndex_App/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         return label
     }()
     
+    
     private let emailField: UITextField = {
         
         let emailField = UITextField()
@@ -28,32 +29,77 @@ class ViewController: UIViewController {
         return emailField
     }()
     
-   /* private let label:  UITextField = {
+    
+    private let passwordField: UITextField = {
         
-        let label = UILabel()
-        label.textAlignment = .center
-        label.text = "Log In"
-        label.font = .systemFont(ofSize: 24, weight: .semibold)
-        return label
+        let passField = UITextField()
+        passField.placeholder = "Password"
+        passField.layer.borderWidth = 1
+        passField.isSecureTextEntry = true
+        passField.layer.borderColor = UIColor.black.cgColor
+        return passField
     }()
     
-    private let label: UIButton = {
+    
+    private let button: UIButton = {
         
-        let label = UILabel()
-        label.textAlignment = .center
-        label.text = "Log In"
-        label.font = .systemFont(ofSize: 24, weight: .semibold)
-        return label
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Continue", for: .normal)
+        return button
     }()
     
-*/
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(label)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+        label.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: 80)
+        
+        emailField.frame = CGRect(x: 20,
+                                  y: label.frame.origin.y+label.frame.size.height+10,
+                                  width: view.frame.size.width-40,
+                                  height: 50)
+        
+        passwordField .frame = CGRect(x: 20,
+                                      y: emailField.frame.origin.y+emailField.frame.size.height+10,
+                                      width: view.frame.size.width-40,
+                                      height: 50)
+        
+        button.frame = CGRect(x: 20,
+                              y: passwordField.frame.origin.y+passwordField.frame.size.height+30,
+                              width: view.frame.size.width-40 ,
+          
+                              height: 52)
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        emailField.becomeFirstResponder()
     }
 
+    
+    @objc private func didTapButton() {
+        
+        print("Continue button tapped")
+        
+    }
+    
+    
 
-    
-    
 }
